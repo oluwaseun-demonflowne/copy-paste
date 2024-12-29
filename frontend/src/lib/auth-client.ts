ebc770543b896e620ef4522b1f12c205bd82f787
demonflowne <ibuemmanuel60@gmail.com>
2024-12-29 23:11:58 +0100

    removed plugins

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,8 +1,8 @@
-import { customSessionClient, multiSessionClient } from "better-auth/client/plugins";
+// import { customSessionClient, multiSessionClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const { useSession } = createAuthClient({
   fetchOptions: { credentials: "include" },
-  plugins: [customSessionClient(), multiSessionClient()],
+  // plugins: [customSessionClient(), multiSessionClient()],
   baseURL:
     process.env.NODE_ENV === "production"
       ? "https://copy-paste-8sxg.onrender.com"
