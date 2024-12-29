e523ff969d4b1440ef403fa76ea2a6979f9f230a
demonflowne <ibuemmanuel60@gmail.com>
2024-12-29 16:53:42 +0100

    checking logs in prof

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,15 +1,19 @@
 import { createAuthClient } from "better-auth/client";
 import { FcGoogle } from "react-icons/fc";
+import { useSession } from "../lib/auth-client";
 
 const Login = () => {
   const authClient = createAuthClient();
+  const { data } = useSession();
+  console.log(data);
   const signIn = async () => {
     const data = await authClient.signIn.social({
       provider: "google",
       fetchOptions: {
-        baseURL: process.env.NODE_ENV === "production"
-        ? "https://copy-paste-8sxg.onrender.com/api/auth"
-        : "http://localhost:5001/api/auth",
+        baseURL:
+          process.env.NODE_ENV === "production"
+            ? "https://copy-paste-8sxg.onrender.com/api/auth"
+            : "http://localhost:5001/api/auth",
         onError: (error) => {
           console.log("error hahah");
           console.log(error.error);
